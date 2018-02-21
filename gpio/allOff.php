c4e5886a960e4aaf8d269c5a759f1f7f524d2562
Paul Fleury <paul.fleuryp@gmail.com>
2018-02-22 00:04:58 +0100

    up

---
+++ gpio/allOff.php
@@ -0,0 +1,4 @@
+<?php shell_exec("sudo python3 10.py");
+shell_exec("sudo python3 20.py");
+shell_exec("sudo python3 30.py");
+shell_exec("sudo vcgencmd display_power 0");?>
---
+++ gpio/allOn.php
@@ -0,0 +1,4 @@
+<?php shell_exec("sudo python3 11.py");
+shell_exec("sudo python3 21.py");
+shell_exec("sudo python3 31.py");
+shell_exec("sudo vcgencmd display_power 1");?>
