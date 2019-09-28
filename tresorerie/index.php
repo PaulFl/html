e49e3317e21da00dea2fca0d239e9a50217643e8
Paul Fleury <paul.fleuryp@gmail.com>
2019-09-29 00:58:45 +0200

    indentation

--- tresorerie/index.php
+++ tresorerie/index.php
@@ -1,21 +1,21 @@
 <?php
-try
-{
-		$bdd = new PDO('mysql:host=localhost;dbname=x3_tresorerie;charset=utf8', 'x3_tresorerie_website', 'x3trezsafe');
-}
-catch(Exception $e)
-{
-	        die('Erreur : '.$e->getMessage());
-}
-
-$reponse = $bdd->query('SELECT nom FROM users LIMIT 0, 10');
-
-echo '<p>Voici les 10 premières entrées de la table users :</p>';
-while ($donnees = $reponse->fetch())
-{
-		echo $donnees['nom'] . '<br />';
-}
-
-$reponse->closeCursor();
-
-?>
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=x3_tresorerie;charset=utf8', 'x3_tresorerie_website', 'x3trezsafe');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+    
+    $reponse = $bdd->query('SELECT nom FROM users LIMIT 0, 10');
+    
+    echo '<p>Voici les 10 premières entrées de la table users :</p>';
+    while ($donnees = $reponse->fetch())
+    {
+        echo $donnees['nom'] . '<br />';
+    }
+    
+    $reponse->closeCursor();
+    
+    ?>
