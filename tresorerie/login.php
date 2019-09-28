3b4528c20b8903ee3393b5077301bd65fff5c710
Paul Fleury <paul.fleuryp@gmail.com>
2019-09-29 00:17:38 +0200

    up

---
+++ tresorerie/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Trésorerie X3</title>
+    </head>
+    <body>
+        <p>Surnom et mot de passe pour acceder à ton compte stp bb</p>
+        <form action="secret.php" method="post">
+            <p>
+            <input type="user" name="user" />
+            <input type="password" name="password" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
+        <p>Acces au trésor du X3</p>
+    </body>
+</html>
