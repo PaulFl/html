7a4f740a4d73da3576ed4cf458a47a3e86d7f509
Paul Fleury <paul.fleuryp@gmail.com>
2019-09-29 00:57:14 +0200

    test

--- tresorerie/password_verif.php
+++ tresorerie/password_verif.php
@@ -20,28 +20,29 @@
     $reponse->closeCursor();
     
     //  Récupération de l'utilisateur et de son pass hashé
-    $req = $bdd->prepare('SELECT surnom, password FROM users WHERE surnom = :user');
-    $req->execute(array(
-                        'user' => $user));
-    $resultat = $req->fetch();
+   // $req = $bdd->prepare('SELECT surnom, password FROM users WHERE surnom = :user');
+   // $req->execute(array(
+   //                     'user' => $user));
+   // $resultat = $req->fetch();
 
-    echo $resultat['password'];
+   // echo $resultat['password'];
     // Comparaison du pass envoyé via le formulaire avec la base
-    $isPasswordCorrect = $_POST['password'] == $resultat['password'];
+   // $isPasswordCorrect = $_POST['password'] == $resultat['password'];
     
-    if (!$resultat)
-    {
-        echo 'Mauvais identifiant ou mot de passe !';
-    }
-    else
-    {
-        if ($isPasswordCorrect) {
-            session_start();
-            $_SESSION['id'] = $resultat['id'];
-            $_SESSION['pseudo'] = $pseudo;
-            echo 'Vous êtes connecté !';
-        }
-        else {
-            echo 'Mauvais identifiant ou mot de passe !';
-        }
-    }
+   // if (!$resultat)
+   // {
+   //     echo 'Mauvais identifiant ou mot de passe !';
+  //  }
+   // else
+  //  {
+    //    if ($isPasswordCorrect) {
+     //       session_start();
+    //        $_SESSION['id'] = $resultat['id'];
+    //        $_SESSION['pseudo'] = $pseudo;
+     //       echo 'Vous êtes connecté !';
+    //    }
+    //    else {
+    //        echo 'Mauvais identifiant ou mot de passe !';
+    //    }
+    //}
+?>
