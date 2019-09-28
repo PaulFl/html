da12aad9582863d1b0dcdca1722779418a863591
Paul Fleury <paul.fleuryp@gmail.com>
2019-09-29 00:35:04 +0200

    nice login page

--- tresorerie/login.php
+++ tresorerie/login.php
@@ -7,12 +7,15 @@
     <body>
         <p>Surnom et mot de passe pour acceder à ton compte stp bb</p>
         <form action="secret.php" method="post">
-            <p>
-            <input type="user" name="user" />
-            <input type="password" name="password" />
-            <input type="submit" value="Valider" />
+	    <p>
+		Surnom: 
+	    <input type="user" name="user" />
+		<br>
+		MDP: 
+	    <input type="password" name="password" />
+		<br>
+            <input type="submit" value="Ça part" />
             </p>
         </form>
-        <p>Acces au trésor du X3</p>
     </body>
 </html>
