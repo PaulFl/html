20b511698576857d3aeba086a1f7b3b735c51fd1
Paul Fleury <paul.fleuryp@gmail.com>
2019-09-28 11:15:38 +0200

    working database

---
+++ x3_tresorerie/db_connection.php
@@ -0,0 +1,22 @@
+<?php
+
+function OpenCon()
+ {
+	  $dbhost = "localhost";
+	   $dbuser = "paul";
+	   $dbpass = "paulflry";
+	    $db = "x3_tresorerie";
+
+
+	    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+	     
+	     return $conn;
+	     }
+ 
+function CloseCon($conn)
+	 {
+		  $conn -> close();
+		   }
+   
+?>
---
+++ x3_tresorerie/index.php
@@ -0,0 +1,21 @@
+<?php
+try
+{
+		$bdd = new PDO('mysql:host=localhost;dbname=x3_tresorerie;charset=utf8', 'root', 'paulflry');
+}
+catch(Exception $e)
+{
+	        die('Erreur : '.$e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT nom FROM users LIMIT 0, 10');
+
+echo '<p>Voici les 10 premières entrées de la table users :</p>';
+while ($donnees = $reponse->fetch())
+{
+		echo $donnees['nom'] . '<br />';
+}
+
+$reponse->closeCursor();
+
+?>
