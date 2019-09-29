fb28bdf4dcada948f5a40862e1c8b2953db3f4ad
Paul Fleury <paul.fleuryp@gmail.com>
2019-09-29 10:20:56 +0200

    up

--- tresorerie/db_connection.php
+++ tresorerie/db_connection.php
@@ -5,14 +5,14 @@ function OpenCon()
 	  $dbhost = "localhost";
 	   $dbuser = "paul";
 	   $dbpass = "paulflry";
-	    $db = "x3_tresorerie";
+		$db = "x3_tresorerie";
 
 
-	    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+		$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
-	     
-	     return $conn;
-	     }
+
+		 return $conn;
+		 }
  
 function CloseCon($conn)
 	 {
