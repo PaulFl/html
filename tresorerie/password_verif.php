182541510668affab4b3a6ebdf1012f15a633cc7
Paul Fleury <paul.fleuryp@gmail.com>
2019-09-29 02:16:04 +0200

    up login page

--- tresorerie/password_verif.php
+++ tresorerie/password_verif.php
@@ -41,6 +41,8 @@
         }
     }
     
-    <button type="button">Click Me!</button>
+    <form>
+    <input type="button" value="Put Your Text Here" onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+    </form>
     
     ?>
