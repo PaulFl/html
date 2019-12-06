02ec767a229f81792f722ae03522a631083d0c59
Paul Fleury <paul.fleuryp@gmail.com>
2019-12-06 13:42:21 +0100

    added random picture

--- index.php
+++ index.php
@@ -28,25 +28,48 @@
             <a href="tresorerie/connections.php">Stats</a>
         </td>
     </tr>
-    <tr> <td>
-        <?php
-        try {
-            $bdd = new PDO('mysql:host=localhost;dbname=x3_tresorerie;charset=utf8', 'x3_tresorerie_website', 'x3trezsafe');
-        } catch (Exception $e) {
-            die('Erreur : ' . $e->getMessage());
-        }
-        $resultat = $bdd->query('SELECT COUNT(id) as qty from transactions');
-        $transactions_qty = $resultat->fetch()['qty'];
-        $resultat = $bdd->query('SELECT SUM(montant) as somme from transactions');
-        $transactions_sum = $resultat->fetch()['somme'];
-        $transactions_sum = round($transactions_sum, 2);
-        echo $transactions_qty . ' dettes pour ' . $transactions_sum . '€';
-        ?>
+    <tr>
+        <td>
+            <?php
+            try {
+                $bdd = new PDO('mysql:host=localhost;dbname=x3_tresorerie;charset=utf8', 'x3_tresorerie_website', 'x3trezsafe');
+            } catch (Exception $e) {
+                die('Erreur : ' . $e->getMessage());
+            }
+            $resultat = $bdd->query('SELECT COUNT(id) as qty from transactions');
+            $transactions_qty = $resultat->fetch()['qty'];
+            $resultat = $bdd->query('SELECT SUM(montant) as somme from transactions');
+            $transactions_sum = $resultat->fetch()['somme'];
+            $transactions_sum = round($transactions_sum, 2);
+            echo $transactions_qty . ' dettes pour ' . $transactions_sum . '€';
+            ?>
         </td>
     </tr>
 </table>
 <br><br>
 <b> Cliques sur la photo pour parler business...</b> <br><br>
-<a href="tresorerie/login.php"><img src="zahfy.jpg" style="max-width:100%;" alt="Etage roi"/>
+<?php
+$i = 0;
+$path = "x3_photos";
+$ext = "jpeg";
+$extra = "alt=\"Random Image\" float=\"left\"";
+$imgs = [];
+if ($handle = opendir($path)) {
+    while (false !== ($file = readdir($handle))) {
+        if (substr($file, strlen($file) - 4, 4) == $ext) {
+            $imgs[$i++] = $file;
+        }
+    }
+    closedir($handle);
+    $today = getdate();
+    srand($today['mday'] + $today['mon']);
+
+    $r = rand(0, $i - 1);
+
+    echo "<a href=\"tresorerie/login.php\"><img src=\"x3_photos/{$imgs[$r]}\" style=\"max-width:100%;\" alt=\"Etage roi\"/>";
+
+}
+?>
+<!--<a href="tresorerie/login.php"><img src="zahfy.jpg" style="max-width:100%;" alt="Etage roi"/>-->
 </body>
 </html>
