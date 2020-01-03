fc55dfb1e9253f37f4b1310a89ad4a5ab41211c6
Paul Fleury <paul.fleuryp@gmail.com>
2020-01-03 19:36:32 +0100

    changed mail header

--- tresorerie/mail_test.php
+++ tresorerie/mail_test.php
@@ -2,5 +2,5 @@
 $to_email = 'paul@fleury.io';
 $subject = 'Dette trésorerie X3';
 $message = 'This mail is sent using the PHP mail function';
-$headers = 'From: trez@x3lesang.fleury.io';
-mail($to_email,$subject,$message,$headers);
+$headers = 'From: tresorerie.paul.fleuryp@gmail.com';
+echo mail($to_email,$subject,$message,$headers);
